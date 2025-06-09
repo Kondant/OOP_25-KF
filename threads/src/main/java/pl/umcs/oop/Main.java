98dcaf07f9c58cd984dbcfbc4dd478ada7dfb5b9
Kondant <konradflis05@gmail.com>
2025-06-09 18:59:56 +0200

    Wielowątkowość NA PLUSY

--- threads/src/main/java/pl/umcs/oop/Main.java
+++ threads/src/main/java/pl/umcs/oop/Main.java
@@ -1,28 +1,42 @@
 package pl.umcs.oop;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        ImageHandler ih=new ImageHandler();
-        ih.loadImage("C:\\Users\\student\\Desktop\\test.png");
+    public static void main(String[] args) throws IOException {
+        ImageHandler ih = new ImageHandler();
+
+        ih.loadImage("C:\\Users\\konra\\Desktop\\test.png");
         long start = System.currentTimeMillis();
         ih.increaseBrightness(0x77);
-        long end=System.currentTimeMillis();
-        ih.saveImage("C:\\Users\\student\\Desktop\\test.png");
-        System.out.println("czas 1 watku: "+(end-start)+"ms");
+        long end = System.currentTimeMillis();
+        ih.saveImage("out.png");
+        System.out.println("czas na 1 wątku: "+(end-start) + "ms");
 
-        ih.loadImage("C:\\Users\\student\\Desktop\\test.png");
+        ih.loadImage("C:\\Users\\konra\\Desktop\\test.png");
         long start2 = System.currentTimeMillis();
-        ih.increaseBrightnessThreaded(0x77);
-        long end2=System.currentTimeMillis();
-        ih.saveImage("C:\\Users\\student\\Desktop\\test.png");
-        System.out.println("czas 1 watku: "+(end2-start2)+"ms");
+        ih.increaseBrightnessMultiThreaded(0x77);
+        long end2 = System.currentTimeMillis();
+        ih.saveImage("out.png");
+        System.out.println("czas na wielu wątkach: "+(end2-start2) + "ms");
 
-        ih.loadImage("C:\\Users\\student\\Desktop\\test.png");
+        ih.loadImage("C:\\Users\\konra\\Desktop\\test.png");
         long start3 = System.currentTimeMillis();
         ih.increaseBrightnessThreadPool(0x77);
-        long end3=System.currentTimeMillis();
-        ih.saveImage("C:\\Users\\student\\Desktop\\test.png");
-        System.out.println("czas wielu watkow: "+(end3-start3)+"ms");
+        long end3 = System.currentTimeMillis();
+        ih.saveImage("out.png");
+        System.out.println("czas na wielu wątkach: "+(end3-start3) + "ms");
+
+        ih.loadImage("C:\\Users\\konra\\Desktop\\test.png");
+        int[] histogram = ih.makeHistogram("R");
+        for (int i = 0; i < histogram.length; i++) {
+            System.out.println("R[" + i + "] = " + histogram[i]);
+        }
+        BufferedImage histImage = ImageHandler.HistogramRenderer.generateHistogramImage(histogram);
+        ImageIO.write(histImage, "png", new File("histogram_output.png"));
+
     }
 }
